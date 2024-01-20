b96093cc016c897872362b847f3654b2bd905c52
Silvana Melitsko <silvana.melitsko@gmail.com>
2024-01-20 10:04:29 -0300

    actualizacion

---
+++ src/org/ada/dominio/Perro.java
@@ -0,0 +1,15 @@
+package org.ada.dominio;
+
+public class Perro {
+    String raza;
+    int edad;
+    boolean esDomestico;
+    double peso;
+
+    String nombre;
+
+    public void decirNombre(){
+        System.out.println("Mi nombre es " + nombre);
+
+    }
+}
